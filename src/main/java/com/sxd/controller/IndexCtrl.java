e697b364e1b7614795e07f1d589aaccf532fa4f4
jhk314 <776375962@qq.com>
2021-03-08 12:32:38 +0800

    push

--- src/main/java/com/sxd/controller/IndexCtrl.java
+++ src/main/java/com/sxd/controller/IndexCtrl.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/index")
 public class IndexCtrl {
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        request.setAttribute("sysName","后台服务系统");
         return "index.html";
     }
 }
